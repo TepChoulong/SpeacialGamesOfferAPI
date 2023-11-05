08164834317ee117184db81341533bd7b8bcba73
TepChoulong <tepchoulongzin@gmail.com>
2023-11-05 20:24:03 +0700

    Fix some bugs already

--- index.js
+++ index.js
@@ -36,18 +36,12 @@ const GameWebsites = [
         name: 'GOG',
         address: 'https://www.gog.com/en/games?page=5',
         base: 'https://www.gog.com/en/'
-    },
-    {
-        name: 'EpicGame',
-        address: 'https://store.epicgames.com/en-US/browse?sortBy=releaseDate&sortDir=DESC&priceTier=tierDiscouted&category=Game&count=40&start=0',
-        base: 'https://store.epicgames.com/en-US/'
     }
 ]
 
 // .sc-dkQUaI
 
 let All_Games = [];
-const EpicGames_List = [];
 const GOG_Games = [];
 const Steam_Games = [];
 
@@ -93,16 +87,20 @@ GameWebsites.forEach(gameWebsites => {
                 })
             })
 
-            // EpicGame
-            $('.css-lrwy1y', html).each(function() {
+            $('product-tile', html).each(function() {
 
-                const title = $(this).find('.css-rgqwpc').text();
+                const title = $(this).find('.small').find('span').text();
+                const base_prices = $(this).find('.ng-star-inserted').find('.base-value').text();
+                const final_prices = $(this).find('.ng-star-inserted').find('.final-value').text();
+                const discount_percentage = $(this).find('price-discount').text();
 
-                EpicGames_List.push({
-                    title
+                GOG_Games.push({
+                    title,
+                    base_prices,
+                    final_prices,
+                    discount_percentage
                 })
             })
-
         })
         .catch((err) => {
 
@@ -118,10 +116,6 @@ app.get('/steam-special-discount-games', (req, res) => {
 
 app.get('/gog-special-discount-games', (req, res) => {
     res.json(GOG_Games);
-})
-
-app.get('/epicgame-special-discount-games', (req, res) => {
-    res.json(EpicGames_List);
 })
 
 
